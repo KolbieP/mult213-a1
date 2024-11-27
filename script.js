36cad7813a1e7c2bf54f1b655e22a1ea8bc0b6bb
Kolbie <kolbierparker20@outlook.com>
2024-11-27 11:09:36 -0600

    Updates
    -Updated script and html code
    -Not working yet

--- script.js
+++ script.js
@@ -1,66 +1,67 @@
 
-/*
-const fetchSpells = async () => {
-    const res = await fetch('https://potterapi-fedeperin.vercel.app/en/spells')
-    const spells = await res.json()
-
-    return spells
-}
-//fetch('https://potterapi-fedeperin.vercel.app/es/characters?search=Weasley')
-fetch('https://potterapi-fedeperin.vercel.app/es/spells?')
-    .then(res => res.json())
-    .then(res => {
-        console.log(res)
-    })
-*/
-
-let hogwarts_id = -1;
-
-
 // Form Submission
 document.getElementById('hogwartsForm').addEventListener('submit', event => {
     event.preventDefault(); // Prevent the form from reloading the page
-    const hogwartsName = document.getElementById('hogwartsName').value.trim().toLowerCase();
-    fetchHouse(hogwartsName);
+    const houseName = document.getElementById('houseName').value.trim().toLowerCase();
+    fetchHouse(houseName);
 });
 
-
-document.querySelector("#gethouse").addEventListener('click', event => {
-    console.log('Hogwarts House Pending...');
-    // fetch Hogwarts house that is +1 from id
-    if(hogwarts_id < 0) {
-        console.log('Something went wrong!'); 
+/*
+document.querySelector("#forward").addEventListener('click', event => {
+    console.log('forward button was clicked');
+    // fetch pokemon that is +1 from id
+    if(pokemon_id < 0) {
+        console.log('please search for a pokemon first');
     } else {
-        fetchHouse(hogwarts_id);
-        console.log(hogwarts_id);
+        fetchPokemon(pokemon_id + 1);
+        console.log(pokemon_id + 1);
     }
 });
-    
-const fetchHouse = (name) => {
-    // Using a promise to fetch the Hogwarts data
+
+
+document.querySelector("#back").addEventListener('click', event => {
+    console.log('back button was clicked');
+    // fetch pokemon that is -1 from id
+    if(pokemon_id < 0) {
+        console.log('please search for a pokemon first');
+    } else {
+        console.log(pokemon_id);
+        fetchPokemon(pokemon_id - 1);
+
+    }
+});
+
+*/
+
+const fetchHouse = (houseName) => {
+    // Using a promise to fetch the Pokémon data
     // https://pokeapi.co/api/v2/pokemon/pikachu for example
+    //fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    //fetch(`https://potterapi-fedeperin.vercel.app/en/${houseName}`)
     fetch(`https://potterapi-fedeperin.vercel.app/en/houses`)
         .then(response => {
             if (!response.ok) {
-                throw new Error("House Not Found");
+                throw new Error("House not found");
             }
             return response.json();
         })
         .then(data => {
             console.log(data);
-            HogwartsHouseDisplay(data);
+            displayHouse(data);
         })
         .catch(error => {
             console.log(error);
         });
 }
 
-
-const HogwartsHouseDisplay = (hogwarts) => {
-    hogwarts_id = hogwarts.id;
-    // Display the Hogwarts House data
-    document.getElementById('HogwartsImage').src = emoji;
-    document.getElementById('HogwartsImage').hidden = false;
-    document.getElementById('HogwartsInfo').textContent = `#${hogwarts.house} ${hogwarts.colors} ${hogwarts.animal}`;
+const displayHouse = (houseName) => {
+    house_id = house.id;
+    // Display the Pokémon data
+    //document.getElementById('houseImage').src = emoji;
+    //document.getElementById('houseImage').hidden = false;
+    document.getElementById('houseInfo').textContent = `#${houseName.house} ${houseName.animal} ${houseName.colors}`;
 }
+
+
+
     
